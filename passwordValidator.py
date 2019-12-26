80b5234b90e744ca25154ef67367c720d9cabde8
Muhammad Ali <alijklmno@gmail.com>
2019-12-26 23:06:33 +0500

    New Implementation

--- passwordValidator.py
+++ passwordValidator.py
@@ -1,3 +1,5 @@
+from string import digits as ints
+from string import punctuation as spChars
 #Takes user input that will be the password to be validated
 userInput = input("Enter a password to validate: ")
 
@@ -5,10 +7,6 @@ userInput = input("Enter a password to validate: ")
 is7Characters = False
 has2Integers = False
 has2SpecialChars = False
-
-#Initializing List of Special Characters and Integers
-specialCharacters = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/']
-integerValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 #Converting user input string to list for each character manipulation
 userList = list(userInput)
@@ -18,23 +16,12 @@ if len(userList) >= 7:
     is7Characters = True
 
 #Condition check if the number of special characters is more than 2
-spCount = 0
-for i in userList:
-    for j in specialCharacters:
-        if i == j:
-            spCount += 1
+if len(set(spChars) & set(userList)) >= 2:
+    has2SpecialChars = True
 
 #Condition check if the number of integers is more than 2
-intCount = 0
-for i in userList:
-    for j in integerValues:
-        if i == j:
-            intCount += 1
-
-#Setting flags to true if intergers are 2 and special characters are 2 in the passsword
-if (spCount >= 2 and intCount >=2):
+if len(set(ints) & set(userList)) >= 2:
     has2Integers = True
-    has2SpecialChars = True
 
 #Condition check for all the flags to be true if yes, then print out strong else print out weak
 if (is7Characters == True and has2Integers == True and has2SpecialChars == True):
